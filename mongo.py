6b15a919d3a739d1b569198de138adcafbd05647
Yash Gupta <yash.gupta755@gmail.com>
2016-10-01 11:44:40 +0530

    Task

--- mongo.py
+++ mongo.py
@@ -3,19 +3,19 @@ from pymongo.errors import ConnectionFailure
 import task
 
 try:
-	client = MongoClient()
-	db = client.provakil
+    client = MongoClient()
+    db = client.provakil
 except ConnectionFailure as e:
-	print "Could not connect to mongo instance"
-	print repr(e)
+    print "Could not connect to mongo instance"
+    print repr(e)
 
-input_list = task.inputs()	
-lists = task.get_case_status(input_list[0],input_list[1],input_list[2])
+input_list = task.inputs()
+lists = task.get_case_status(input_list[0], input_list[1], input_list[2])
 db.cases.drop()
 if not lists:
-             print "Sorry ! No data found"
+    print "Sorry ! No data found"
 else:
-	for case in lists:
-		db.cases.insert(case)
+    for case in lists:
+        db.cases.insert(case)
 
-	print "Data Inserted!!!!"
+    print "Data Inserted!!!!"
